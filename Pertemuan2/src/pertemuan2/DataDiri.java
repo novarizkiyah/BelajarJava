f7e1daa654ec08c9fddcf57ba9b97e507717f081
novarizkiyah <novalailatulrizkiyah@gmail.com>
2024-05-30 20:12:38 +0200

    kedua

--- Pertemuan2/src/pertemuan2/DataDiri.java
+++ Pertemuan2/src/pertemuan2/DataDiri.java
@@ -9,5 +9,27 @@ package pertemuan2;
  * @author nova
  */
 public class DataDiri {
+    public static void main(String[] args){
+    // membuat variabel
+    String nama, alamat;
+    int usia;
+    double tinggi;
     
+    //Mengisi variabel
+    nama = "Nova";
+    alamat = "Kawi";
+    usia = 30;
+    tinggi = 155.5;
+    
+    //mencetak ke layar
+    System.out.println("Nama saya adalah " + nama);
+    System.out.println("Alamar saya adalah " + alamat);
+    System.out.println("Usia saya adalah " + usia + "tahun");
+    System.out.println("Tinggi saya adalah " + tinggi + "cm");
+          
+            
+            
+            
+        
+    }
 }
